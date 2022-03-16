2fb7521e0bcf4ad7478fdb24e7a72f8d17352a3b
Johan Fahlgren <johan.fahlgren@utb.ecutbildning.se>
2022-03-16 14:07:44 +0100

    Razor pages and bootstrap

--- MyCrafts.WebSite/Pages/Index.cshtml.cs
+++ MyCrafts.WebSite/Pages/Index.cshtml.cs
@@ -1,20 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using MyCrafts.WebSite.Models;
+using MyCrafts.WebSite.Services;
 
 namespace MyCrafts.WebSite.Pages
 {
     public class IndexModel : PageModel
     {
         private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product>? Products { get; private set; }
 
-        public IndexModel(ILogger<IndexModel> logger)
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService)
         {
             _logger = logger;
+            ProductService = productService;
+
         }
 
         public void OnGet()
         {
-
+            Products = ProductService.GetProducts();
         }
     }
 }
